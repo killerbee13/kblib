052b909c17002aabe98a0f1b9c56f06e00b31441
killerbee <killerbee13.0197@gmail.com>
2021-03-08 02:47:15 -0800

    Fixed live_ptr-related segfaults, and reduced repetition by using CRTP.

--- kblib/memory.h
+++ kblib/memory.h
@@ -34,212 +34,8 @@ struct fun_ptr_deleter<FunPtr> {
 };
 #endif
 
-template <typename T>
-class live_ptr;
-
-template <typename T>
-class live_wrapper {
- public:
-	// no constructors to make it an aggregate
-	~live_wrapper() {
-		for (auto p : _observers) {
-			if (p) {
-				*p = nullptr;
-			}
-		}
-	}
-
-	live_ptr<T> ref();
-	live_ptr<const T> ref() const;
-	live_ptr<const T> cref() const;
-
-	T data;
-
-	kblib::null_construct<std::vector<live_wrapper**>> _observers{};
-};
-
-template <typename T>
-class live_wrapper<const T> : public live_wrapper<T> {};
-
-template <typename T>
-class live_ptr {
- public:
-	live_ptr() = default;
-	live_ptr(const live_ptr& o) : obj(o.obj) { add(); }
-	live_ptr(live_ptr&& o) noexcept : obj(std::exchange(o.obj, nullptr)) {
-		if (obj) {
-			std::replace(obj->_observers.begin(), obj->_observers.end(), &o.obj,
-			             &obj);
-		}
-	}
-	explicit live_ptr(live_wrapper<T>& o) : obj(&o) { add(); }
-
-	live_ptr& operator=(const live_ptr& o) {
-		rem();
-		obj = o.obj;
-		add();
-		return *this;
-	}
-	live_ptr& operator=(live_ptr&& o) noexcept {
-		rem();
-		if ((obj = std::exchange(o.obj, nullptr))) {
-			std::replace(obj->_observers.begin(), obj->_observers.end(), &o.obj,
-			             &obj);
-		}
-		return *this;
-	}
-	live_ptr& operator=(live_wrapper<T>& o) {
-		rem();
-		obj = &o;
-		add();
-		return *this;
-	}
-
-	T& operator*() noexcept { return obj->data; }
-	const T& operator*() const noexcept { return obj->data; }
-
-	T* operator->() noexcept { return &obj->data; }
-	const T* operator->() const noexcept { return &obj->data; }
-
-	operator bool() const noexcept { return obj; }
-
-	friend bool operator==(const live_ptr& lhs, std::nullptr_t) {
-		return not lhs;
-	}
-	friend bool operator==(std::nullptr_t, const live_ptr& rhs) {
-		return not rhs;
-	}
-
-	friend bool operator==(const live_ptr& lhs, const live_ptr& rhs) {
-		return lhs.obj == rhs.obj;
-	}
-
-	friend bool operator==(const live_ptr& lhs, const T* rhs) {
-		if (not lhs and not rhs) {
-			return true;
-		} else {
-			return lhs and &lhs.obj->data == rhs;
-		}
-	}
-	friend bool operator==(const T* lhs, const live_ptr& rhs) {
-		if (not rhs and not lhs) {
-			return true;
-		} else {
-			return rhs and &rhs.obj->data == lhs;
-		}
-	}
-
- private:
-	void add() { obj->_observers.push_back(&obj); }
-	void rem() {
-		if (obj) {
-			erase(obj->_observers, &obj);
-		}
-	}
-
-	mutable live_wrapper<T>* obj = nullptr;
-
-	friend class live_ptr<const T>;
-};
-
-template <typename mT>
-class live_ptr<const mT> {
- public:
-	using T = const mT;
-	live_ptr() = default;
-	live_ptr(const live_ptr<T>& o) : obj(o.obj) { add(); }
-	live_ptr(const live_ptr<mT>& o) : obj(o.obj) { add(); }
-	live_ptr(live_ptr<T>&& o) : obj(o.pop()) { add(); }
-	live_ptr(live_ptr<mT>&& o) : obj(o.pop()) { add(); }
-	explicit live_ptr(live_wrapper<T>& o) : obj(&o) { add(); }
-	explicit live_ptr(live_wrapper<mT>& o) : obj(&o) { add(); }
-
-	live_ptr& operator=(const live_ptr& o) {
-		rem();
-		obj = o.obj;
-		add();
-		return *this;
-	}
-	live_ptr& operator=(live_ptr&& o) noexcept {
-		rem();
-		if ((obj = std::exchange(o.obj, nullptr))) {
-			std::replace(obj->_observers.begin(), obj->_observers.end(), &o.obj,
-			             &obj);
-		}
-		return *this;
-	}
-	live_ptr& operator=(live_wrapper<T>& o) {
-		rem();
-		obj = &o;
-		add();
-		return *this;
-	}
-
-	T& operator*() noexcept { return *obj->data; }
-	const T& operator*() const noexcept { return *obj->data; }
-
-	T* operator->() noexcept { return obj->data; }
-	const T* operator->() const noexcept { return obj->data; }
-
-	operator bool() const noexcept { return obj; }
-
-	friend bool operator==(const live_ptr& lhs, std::nullptr_t) {
-		return not lhs;
-	}
-	friend bool operator==(std::nullptr_t, const live_ptr& rhs) {
-		return not rhs;
-	}
-
-	friend bool operator==(const live_ptr& lhs, const live_ptr& rhs) {
-		return lhs.obj == rhs.obj;
-	}
-
-	friend bool operator==(const live_ptr& lhs, const T* rhs) {
-		if (not lhs and not rhs) {
-			return true;
-		} else {
-			return lhs and &lhs.obj->data == rhs;
-		}
-	}
-	friend bool operator==(const T* lhs, const live_ptr& rhs) {
-		if (not rhs and not lhs) {
-			return true;
-		} else {
-			return rhs and &rhs.obj->data == lhs;
-		}
-	}
-
- private:
-	void add() { obj->_observers.push_back(&obj); }
-	void rem() {
-		if (obj) {
-			erase(obj->_observers, &obj);
-		}
-	}
-	live_wrapper<mT>* pop() {
-		rem();
-		return std::exchange(obj, nullptr);
-	}
-
-	mutable live_wrapper<mT>* obj = nullptr;
-};
-
-template <typename T>
-live_ptr<T> live_wrapper<T>::ref() {
-	return live_ptr<T>{*this};
-}
-
-template <typename T>
-live_ptr<const T> live_wrapper<T>::ref() const {
-	return live_ptr<T>{*this};
-}
-
-template <typename T>
-live_ptr<const T> live_wrapper<T>::cref() const {
-	return live_ptr<T>{*this};
-}
-
 namespace detail {
+
 	template <typename D, typename T, typename V = void>
 	struct filter_deleter_pointer {
 		using type = T*;
@@ -300,7 +96,224 @@ namespace detail {
 		T& base() noexcept { return base_; }
 		const T& base() const noexcept { return base_; }
 	};
+
+	struct noop {
+		void operator()() const noexcept {}
+	};
+	struct value_init {};
+
+	template <typename T, typename Construct = noop, typename Destroy = noop>
+	struct rule_zero : as_base_class<T> {
+		template <typename... Args>
+		rule_zero(Args&&... args)
+		    : as_base_class<T>(std::forward<Args>(args)...) {}
+	};
+
+	template <typename T, typename D>
+	struct on_destroy : as_base_class<T>, as_base_class<D> {
+		~on_destroy() { static_cast<D&> (*this)(static_cast<T&>(*this)); }
+	};
+
 } // namespace detail
+
+template <typename T>
+class live_ptr;
+
+template <typename T>
+class live_wrapper {
+ public:
+	// no constructors to make it an aggregate
+	~live_wrapper() {
+		for (auto p : _observers) {
+			if (p) {
+				*p = nullptr;
+			}
+		}
+	}
+
+	live_ptr<T> ref();
+	live_ptr<const T> ref() const;
+	live_ptr<const T> cref() const;
+
+	T data;
+
+	kblib::null_construct<std::vector<live_wrapper**>> _observers{};
+};
+
+template <typename T>
+class live_wrapper<const T> : public live_wrapper<T> {};
+
+namespace detail {
+
+	template <typename T>
+	struct template_param;
+	template <template <typename T> typename C, typename T>
+	struct template_param<C<T>> {
+		using type = T;
+	};
+
+	template <typename D>
+	struct live_ptr_base {
+	 private:
+		using T = typename template_param<D>::type;
+		using mT = typename std::remove_const<T>::type;
+
+	 public:
+		T& operator*() noexcept { return obj->data; }
+		const T& operator*() const noexcept { return obj->data; }
+
+		T* operator->() noexcept { return &obj->data; }
+		const T* operator->() const noexcept { return &obj->data; }
+
+		operator bool() const noexcept { return obj; }
+
+		friend bool operator==(const D& lhs, std::nullptr_t) { return not lhs; }
+		friend bool operator==(std::nullptr_t, const D& rhs) { return not rhs; }
+
+		friend bool operator==(const D& lhs, const D& rhs) {
+			return lhs.obj == rhs.obj;
+		}
+
+		friend bool operator==(const D& lhs, const T* rhs) {
+			if (not lhs and not rhs) {
+				return true;
+			} else {
+				return lhs and &lhs.obj->data == rhs;
+			}
+		}
+		friend bool operator==(const T* lhs, const D& rhs) {
+			if (not rhs and not lhs) {
+				return true;
+			} else {
+				return rhs and &rhs.obj->data == lhs;
+			}
+		}
+
+		live_ptr_base() noexcept = default;
+		live_ptr_base(live_wrapper<mT>* p) : obj{p} { add(); }
+		live_ptr_base& operator=(const live_ptr_base& o) noexcept {
+			rem();
+			obj = o.obj;
+			add();
+			return *this;
+		}
+		live_ptr_base& operator=(live_ptr_base&& o) noexcept {
+			rem();
+			move(o);
+			return *this;
+		}
+
+		live_ptr_base(const live_ptr_base& o) : obj{o.obj} { add(); }
+		live_ptr_base(live_ptr_base&& o) noexcept { move(o.as_D()); }
+		~live_ptr_base() { rem(); }
+
+		D& operator=(const D& o) {
+			rem();
+			obj = o.obj;
+			add();
+			return as_D();
+		}
+		D& operator=(D&& o) noexcept {
+			this->rem();
+			this->move(o);
+			return as_D();
+		}
+
+	 protected:
+		void add() { obj->_observers.push_back(&obj); }
+		void rem() {
+			if (obj) {
+				erase(obj->_observers, &obj);
+			}
+		}
+		void move(D& o) {
+			if ((obj = std::exchange(o.obj, nullptr))) {
+				std::replace(obj->_observers.begin(), obj->_observers.end(), &o.obj,
+				             &obj);
+			}
+		}
+		mutable live_wrapper<mT>* obj = nullptr;
+
+	 private:
+		D& as_D() noexcept { return static_cast<D&>(*this); }
+		const D& as_D() const noexcept { return static_cast<const D&>(*this); }
+	};
+} // namespace detail
+
+template <typename T>
+class live_ptr : public detail::live_ptr_base<live_ptr<T>> {
+	using base = detail::live_ptr_base<live_ptr<T>>;
+
+ public:
+	using value_type = T;
+
+	live_ptr() = default;
+	live_ptr(const live_ptr& o) = default;
+	live_ptr(live_ptr&& o) noexcept = default;
+	live_ptr& operator=(const live_ptr& o) = default;
+	live_ptr& operator=(live_ptr&& o) noexcept = default;
+
+	explicit live_ptr(live_wrapper<T>& o) : base{&o} { this->add(); }
+	live_ptr& operator=(live_wrapper<T>& o) {
+		this->rem();
+		this->obj = &o;
+		this->add();
+		return *this;
+	}
+
+ private:
+	friend class live_ptr<const T>;
+};
+
+template <typename mT>
+class live_ptr<const mT> : public detail::live_ptr_base<live_ptr<const mT>> {
+	using base = detail::live_ptr_base<live_ptr<const mT>>;
+
+ public:
+	using T = const mT;
+	using value_type = T;
+	live_ptr() = default;
+	live_ptr(const live_ptr<T>& o) = default;
+	live_ptr(const live_ptr<mT>& o) : base{o.obj} {}
+	live_ptr(live_ptr<T>&& o) noexcept = default;
+	live_ptr(live_ptr<mT>&& o) noexcept { this->move(o); }
+	live_ptr& operator=(const live_ptr<T>& o) = default;
+	live_ptr& operator=(live_ptr<T>&& o) noexcept = default;
+
+	live_ptr& operator=(const live_ptr<mT>& o) {
+		this->obj = o.obj;
+		this->add();
+		return *this;
+	}
+	live_ptr& operator=(live_ptr<mT>&& o) noexcept {
+		this->obj = o.obj;
+		this->add();
+		return *this;
+	}
+
+	explicit live_ptr(const live_wrapper<mT>& o) : base{&o} { this->add(); }
+	live_ptr& operator=(const live_wrapper<mT>& o) {
+		this->rem();
+		this->obj = &o;
+		this->add();
+		return *this;
+	}
+};
+
+template <typename T>
+live_ptr<T> live_wrapper<T>::ref() {
+	return live_ptr<T>{*this};
+}
+
+template <typename T>
+live_ptr<const T> live_wrapper<T>::ref() const {
+	return live_ptr<const T>{*this};
+}
+
+template <typename T>
+live_ptr<const T> live_wrapper<T>::cref() const {
+	return live_ptr<const T>{*this};
+}
 
 // cond_ptr: A pointer which can either uniquely own its referent, or which can
 // be a non-owning reference. Note that custom deleter support is not present;
@@ -329,7 +342,7 @@ class cond_ptr : private detail::as_base_class<Deleter> {
 	                  std::decay_t<Deleter> del = {}) noexcept
 	    : d_base{std::move(del)}, ptr_(p), owns_(owner) {}
 	explicit cond_ptr(T* p, std::decay_t<Deleter> del) noexcept
-	    : d_base{std::move(del)}, ptr_(p), owns_(false) {}
+	    : d_base{std::move(del)}, ptr_(p) {}
 
 	cond_ptr(unique&& p) noexcept
 	    : d_base{p.get_deleter()}, ptr_(p.release()), owns_(ptr_) {}
